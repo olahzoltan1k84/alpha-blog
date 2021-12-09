e3d83cd3a5e0f0fdf07d4303b73e4e9d90ba21fb
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2021-12-09 22:39:53 +0200

    Refacor code using partials and make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,7 +12,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -26,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
@@ -35,9 +33,15 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
