82cc3c8df40fb32fbf8658ebbd4482167d3f97a4
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-01-09 13:40:50 +0200

    Add users create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
+    @Article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
