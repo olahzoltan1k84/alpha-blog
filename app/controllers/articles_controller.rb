e182cb00f3b89b333c55f6cc4907b7f999b4366a
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-01-13 22:54:56 +0200

    Add admin user access

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
   end
 
@@ -43,6 +46,13 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 
 end
